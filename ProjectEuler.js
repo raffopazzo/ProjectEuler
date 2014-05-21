63dbfe39e2353e4e428cca76511c04c9f261e3a7
Raffaele Rossi <rossi.raffaele@gmail.com>
2014-05-21 20:20:31 +0100

    Implemented solution to problem #2: Even Fibonacci numbers

--- ProjectEuler.js
+++ ProjectEuler.js
@@ -1,16 +1,85 @@
+/**
+ * Memoize the given function, that is returns a new function 
+ * which has a cache of all the previously computed values of 
+ * the input function. 
+ */
+function memoize(f) {
+  var cache = {};
+  return function() {
+    var key = arguments[0];
+    for (var i = 1; i < arguments.length; i++) {
+      key += "," + arguments[i];
+    }
+    if (key in cache) return cache[key];
+    var val = f.apply(this, arguments);
+    cache[key] = val;
+    return val;
+  }
+}
+
+/**
+ * Return an array with all the multiples of a given number.
+ * 
+ * @param n   The number to return the multiples of.
+ * @param max The maximum value to return (if a multiple). 
+ */
 function multiples(n, max) {
     var result = [];
     for(var t=n; t<=max; t+=n) result.push(t);
     return result;
 }
 
+/**
+ * Return the sum of two numbers
+ */
 function sum(a, b) { return a + b; }
+
+/**
+ * Return the n-th Fibonacci number.
+ */
+function fibonacci(n) {
+  if (n < 2) return 1;
+  return fibonacci(n-1) + fibonacci(n-2);
+}
+
+fibonacci = memoize(fibonacci);
+
+/**
+ * Return an array of integer values from 0 to n (n excluded).
+ */
+function range(n) {
+  var result = [];
+  for (var i = 0; i < n; i++) result.push(i);
+  return result;
+}
+
+/**
+ * Check whether the given number is even or not.
+ */
+function isEven(n) { return n % 2; }
+
+/**
+ * Check whether the given number is odd or not.
+ */
+function isOdd(n) { return ! isEven(n); }
 
 PROBLEMS = [
     {
         title: "Multiples of 3 and 5",
         solve: function() {
             return multiples(3,999).concat(multiples(5,999)).reduce(sum) - multiples(15,999).reduce(sum);
+        }
+    },
+    {
+        title: "Even Fibonacci numbers",
+        solve: function() {
+          var fibonacci_numbers = [];
+          var i = 0;
+          while (fibonacci(i) <= 4000000) {
+            fibonacci_numbers.push(fibonacci(i));
+            i++;
+          }
+          return fibonacci_numbers.filter(isEven).reduce(sum);
         }
     }
 ];
