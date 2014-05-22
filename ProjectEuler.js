bfcf3f87b54dbf28c102408528586dd384d09f1d
Raffaele Rossi <rossi.raffaele@gmail.com>
2014-05-22 02:24:15 +0100

    Implemented solution to problem #3: Largest prime factor

--- ProjectEuler.js
+++ ProjectEuler.js
@@ -1,4 +1,13 @@
 /**
+ * Add an Array.isEqual method to check whether two arrays 
+ * contain the same values.
+ */
+Array.prototype.isEqual = Array.prototype.isEqual || function(that) {
+  return this.length == that.length
+      && this.every(function(n) { return that.indexOf(n) >= 0; });
+}
+
+/**
  * Memoize the given function, that is returns a new function 
  * which has a cache of all the previously computed values of 
  * the input function. 
@@ -45,6 +54,31 @@ function fibonacci(n) {
 fibonacci = memoize(fibonacci);
 
 /**
+ * Return an array with all the prime factors of a given number.
+ */
+function primeFactors(n) {
+  return divisors(n).filter(isPrime);
+}
+
+/**
+ * Return an array with all the divisors of a given number.
+ */
+function divisors(n) {
+  var result = [1];
+  if (n == 1) return result;
+  result.push(n);
+  var sqrt_n = Math.floor(Math.sqrt(n));
+  for (var i=2; i < sqrt_n; i++) {
+    if (n % i == 0) {
+      result.push(i);
+      result.push(n/i);
+    }
+  }
+  if (n == sqrt_n * sqrt_n) result.push(sqrt_n);
+  return result;
+}
+
+/**
  * Return an array of integer values from 0 to n (n excluded).
  */
 function range(n) {
@@ -62,6 +96,16 @@ function isEven(n) { return n % 2 == 0; }
  * Check whether the given number is odd or not.
  */
 function isOdd(n) { return ! isEven(n); }
+
+/**
+ * Check whether the given number is prime or not.
+ */
+function isPrime(n) {
+  if (n == 1) return false;
+  if (n == 2) return true;
+  if (isEven(n)) return false;
+  return divisors(n).isEqual([1,n]);
+}
 
 PROBLEMS = [
     {
@@ -81,24 +125,29 @@ PROBLEMS = [
           }
           return fibonacci_numbers.filter(isEven).reduce(sum);
         }
+    },
+    {
+        title: "Largest prime factor",
+        solve: function() {
+          return Math.max.apply(this, primeFactors(600851475143));
+        }
     }
 ];
 
 jQuery(document).ready(function() {
     var div_problems = jQuery("#problems");
-    for (var i in PROBLEMS) {
-        div_problems.append(problem(PROBLEMS[i]));
-    }
+
+    PROBLEMS.forEach(function(p) {
+      div_problems.append(problem(p));
+    });
 
     function problem(p) {
         var div = jQuery('<div class="problem"><h1>'+p.title+'</h1></div>');
         var button = jQuery('<button>Soluzione</button>');
+        var solution = jQuery('<p class="solution"></p>');
         div.append(button);
-        button.click(function() {
-            var solution = jQuery('<p class="solution"></p>');
-            solution.text(p.solve());
-            div.append(solution);
-        });
+        div.append(solution);
+        button.click(function() { solution.text(p.solve()); });
         return div;
     }
 });
