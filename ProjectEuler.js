7f464ede11fcf28cb738e42563e6a3837f0618b6
Raffele Rossi <rossi.raffaele@gmail.com>
2014-05-26 14:44:31 +0100

    Implemented solution to problem #11: Largest product in a grid
    Change-Id: Ib287b3028497f79924703b143902679eb4a3125f

--- ProjectEuler.js
+++ ProjectEuler.js
@@ -29,6 +29,19 @@ Array.prototype.cartesianProduct = Array.prototype.cartesianProduct || function(
 }
 
 /**
+ * Return the greatest product in the array.
+ */
+Array.prototype.maxProduct = Array.prototype.maxProduct || function(window_size) {
+    if (this.length < window_size) return Number.MIN_VALUE;
+    var self = this;
+    var max = Number.MIN_VALUE;
+    range(this.length - window_size + 1).map(function(n) {
+      max = Math.max(max, self.slice(n, n + window_size).reduce(multiply));
+    });
+    return max;
+}
+
+/**
  * Add a String.reverse method to reverse a string.
  */
 String.prototype.reverse = String.prototype.reverse || function() {
@@ -38,15 +51,110 @@ String.prototype.reverse = String.prototype.reverse || function() {
 }
 
 /**
+ * Return an array of numbers converting each digit character. 
+ */
+String.prototype.toArrayOfNumbers = String.prototype.toArrayOfNumbers || function() {
+    var result = []
+    Array.prototype.forEach.call(this, function(c) { result.push(+c); });
+    return result;
+}
+
+/**
  * Hide all the enumrable properties of an object's prototype.
  */
-function hideProperties(o) {
+function hideProperties(clazz) {
     var hidden = {enumerable: false };
+    var o = clazz.call({});
     for (i in o) Object.defineProperty(o.constructor.prototype, i, hidden);
 }
 
-hideProperties(new Array());
-hideProperties(new String());
+/**
+ * Constrcutor to build square a matrix.
+ */
+function Matrix(data) {
+    this.data = data;
+}
+
+/**
+ * Retrieve the list of rows in a matrix
+ */
+Matrix.prototype.rows = function() {
+    return this.data;
+}
+
+/**
+ * Retrieve the list of rows in a matrix
+ */
+Matrix.prototype.cols = function() {
+    var result = [];
+    for (var i in range(this.size())) {
+        result[i] = [];
+        for (var j in range(this.size())) {
+            result[i][j] = this.data[j][i];
+        }
+    }
+    return result;
+}
+
+/**
+ * Retrieve all the left diagonals.
+ */
+Matrix.prototype.leftDiagonals = function() {
+    var result = [];
+    for (var i in range(this.size())) {
+        i = Number(i);
+        result[i] = [];
+        if (i > 0) {
+            result[this.size()+i-1] = [];
+        }
+        for (var j in range(this.size()-i)) {
+            j = Number(j);
+            result[i][j] = this.data[j][this.size()-1-j-i];
+            if (i > 0) {
+                result[this.size()+i-1][j] = this.data[i+j][this.size()-1-j];
+            }
+        }
+    }
+
+    return result;
+}
+
+/**
+ * Retrieve all the right diagonals.
+ */
+Matrix.prototype.rightDiagonals = function() {
+    var result = [];
+    for (var i in range(this.size())) {
+        i = Number(i);
+        result[i] = [];
+        if (i > 0) {
+            result[this.size()+i-1] = [];
+        }
+        for (var j in range(this.size()-i)) {
+            j = Number(j);
+            result[i][j] = this.data[i+j][j];
+            if (i > 0) {
+                result[this.size()+i-1][j] = this.data[j][i+j];
+            }
+        }
+    }
+
+    return result;
+
+//    return [[M[i+j][j] for j in range(len(M)-i)] for i in range(len(M))] +\
+//           [[M[j][i+j] for j in range(len(M)-i)] for i in range(len(M))]
+}
+
+/**
+ * Retrieve the size of the matrix (e.g. N in a NxN matrix).
+ */
+Matrix.prototype.size = function() {
+    return this.data.length;
+}
+
+hideProperties(Array);
+hideProperties(String);
+hideProperties(Matrix);
 
 /**
  * Memoize the given function, that is returns a new function 
@@ -331,17 +439,7 @@ PROBLEMS = [
                   + "84580156166097919133875499200524063689912560717606"
                   + "05886116467109405077541002256983155200055935729725"
                   + "71636269561882670428252483600823257530420752963450";
-          var max = 0;
-          var window_size = 13;
-          str.split("0").forEach(function(s) {
-            var numbers = [];
-            Array.prototype.forEach.call(s, function(c) { numbers.push(+c); });
-            if (numbers.length < window_size) return;
-            range(numbers.length - window_size + 1).map(function(n) {
-              max = Math.max(max, numbers.slice(n,n + window_size).reduce(multiply));
-            });
-          });
-          return max;
+          return str.toArrayOfNumbers().maxProduct(13);
         }
     },
     {
@@ -363,6 +461,39 @@ PROBLEMS = [
         title: "Summation of primes",
         solve: function() {
             return primes(2000000).reduce(sum);
+        }
+    },
+    {
+        title: "Largest product in a grid",
+        solve: function() {
+          var grid = new Matrix([[ 8,  2, 22, 97, 38, 15,  0, 40,  0, 75,  4,  5,  7, 78, 52, 12, 50, 77, 91,  8],
+                                 [49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48,  4, 56, 62,  0],
+                                 [81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30,  3, 49, 13, 36, 65],
+                                 [52, 70, 95, 23,  4, 60, 11, 42, 69, 24, 68, 56,  1, 32, 56, 71, 37,  2, 36, 91],
+                                 [22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80],
+                                 [24, 47, 32, 60, 99,  3, 45,  2, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50],
+                                 [32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70],
+                                 [67, 26, 20, 68,  2, 62, 12, 20, 95, 63, 94, 39, 63,  8, 40, 91, 66, 49, 94, 21],
+                                 [24, 55, 58,  5, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72],
+                                 [21, 36, 23,  9, 75,  0, 76, 44, 20, 45, 35, 14,  0, 61, 33, 97, 34, 31, 33, 95],
+                                 [78, 17, 53, 28, 22, 75, 31, 67, 15, 94,  3, 80,  4, 62, 16, 14,  9, 53, 56, 92],
+                                 [16, 39,  5, 42, 96, 35, 31, 47, 55, 58, 88, 24,  0, 17, 54, 24, 36, 29, 85, 57],
+                                 [86, 56,  0, 48, 35, 71, 89,  7,  5, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58],
+                                 [19, 80, 81, 68,  5, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77,  4, 89, 55, 40],
+                                 [ 4, 52,  8, 83, 97, 35, 99, 16,  7, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66],
+                                 [88, 36, 68, 87, 57, 62, 20, 72,  3, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69],
+                                 [ 4, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18,  8, 46, 29, 32, 40, 62, 76, 36],
+                                 [20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74,  4, 36, 16],
+                                 [20, 73, 35, 29, 78, 31, 90,  1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57,  5, 54],
+                                 [ 1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52,  1, 89, 19, 67, 48]]);
+          return ([grid.rows(),
+                   grid.cols(),
+                   grid.leftDiagonals(),
+                   grid.rightDiagonals()].map(function(list) {
+                       return list.map(function(a) {
+                           return a.maxProduct(4);
+                       });
+                   })).map(function(a) { return a.max(); }).max();
         }
     }
 ];
