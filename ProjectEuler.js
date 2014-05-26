72a646a222c1efee68b2697e4043984dc627ad71
Raffaele Rossi <rossi.raffaele@gmail.com>
2014-05-26 21:57:57 +0100

    Implemented solution to problem #17: Number letters count

--- ProjectEuler.js
+++ ProjectEuler.js
@@ -367,6 +367,34 @@ function not(f) {
     }
 }
 
+/**
+ * Return the british english spelling of the given number. 
+ */
+function number_to_letters(n) {
+  if (n > 1000) throw n + " not supported yet";
+  var words = { 0:  "zero",    1:  "one",       2:  "two",      3:  "three",
+                4:  "four",    5:  "five",      6:  "six",      7:  "seven",
+                8:  "eight",   9:  "nine",     10: "ten",      11: "eleven",
+               12: "twelve",  13: "thirteen",  14: "fourteen", 15: "fifteen",
+               16: "sixteen", 17: "seventeen", 18: "eighteen", 19: "nineteen",
+               20: "twenty",  30: "thirty",    40: "forty",    50: "fifty",
+               60: "sixty",   70: "seventy",   80: "eighty",   90: "ninety",
+              100: "hundred", 1000: "thousand"};
+  if (n <= 20) {
+    return words[n];
+  } else if (n < 100) {
+    var tens = Math.floor(n / 10);
+    return words[10 * tens] + ((n % 10 > 0) ? number_to_letters(n % 10) : "");
+  } else if (n < 1000) {
+    var hundreds = Math.floor(n / 100);
+    return number_to_letters(hundreds) + words[100]
+         + ((n % 100 > 0) ? "and" + number_to_letters(n % 100) : "");
+  } else {
+    var thousands = Math.floor(n / 1000);
+    return number_to_letters(thousands) + words[1000];
+  }
+}
+
 PROBLEMS = [
     {
         title: "Multiples of 3 and 5",
@@ -668,6 +696,12 @@ PROBLEMS = [
         title: "Power digit sum",
         solve: function() {
           return bigInt(2).pow(1000).toString().toArrayOfNumbers().reduce(sum);
+        }
+    },
+    {
+        title: "Number letters count",
+        solve: function() {
+          return range(1,1001).map(number_to_letters).reduce(sum).length;
         }
     }
 ];
